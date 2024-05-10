84eaf59e4554828f0b91ef6e26a85ba148886d02
sillygabe <gabrielonline630@gmail.com>
2024-05-10 21:28:42 +0300

    This is the worst code ive ever written, fix later

--- src/listener.rs
+++ src/listener.rs
@@ -1,34 +1,101 @@
-use anyhow::Result;
+use anyhow::{Error, Result};
 use std::{
-    io::{prelude::*, BufReader},
-    net::TcpListener,
+    collections::HashMap, io::{prelude::*, BufReader}, net::TcpListener
 };
+use serde_json::to_string;
+
+type Map = HashMap<String, String>;
 
 pub struct Listener {
     tcp: TcpListener,
+    handlers: HashMap<String, fn(Map) -> Map>
 }
 
 impl Listener {
     pub fn new(ip: &str, port: i64) -> Result<Self> {
-        let local_ip = format!("{ip}:{port}");
+        let new_ip = ip.replace("\"", "");
+        let local_ip = format!("{new_ip}:{port}");
         Ok(Self {
             tcp: TcpListener::bind(local_ip)?,
+            handlers: HashMap::new()
         })
+    }
+
+    pub fn attach_handler(&mut self, case: String, handler: fn(Map) -> Map) {
+        self.handlers.insert(case, handler);
+    }
+    
+    pub fn choose_handler(&self, path: &String) -> Option<&fn(Map) -> Map> {
+        let handler = self.handlers.get(path.as_str());
+        match handler {
+            None => None,
+            Some(_) => handler
+        }
+    }
+
+    fn parse_http_request(request: &str) -> Option<(String, Map)> {
+        let parts: Vec<&str> = request.splitn(2, ' ').collect();
+        if parts.len() != 2 {
+            return None;
+        }
+    
+        let (path_and_query, _version) = (parts[1], parts[0]);
+    
+        let mut path = String::new();
+        let mut query_params = HashMap::new();
+    
+        if let Some(index) = path_and_query.find('?') {
+            path = path_and_query[..index].to_string();
+            let query = &path_and_query[index + 1..];
+    
+            for param in query.split('&') {
+                let key_value: Vec<&str> = param.split('=').collect();
+                if key_value.len() == 2 {
+                    query_params.insert(key_value[0].to_string(), key_value[1].to_string());
+                }
+            }
+        } else {
+            path = path_and_query.split(' ').nth(0).unwrap().to_string();
+        }
+    
+        Some((path, query_params))
     }
 
     pub fn handle_connections(&self) -> Result<()> {
         for stream in self.tcp.incoming() {
-            let stream = stream?;
+            let mut stream = stream?;
 
-            let reader = BufReader::new(stream);
+            let reader = BufReader::new(&stream);
 
             let http_request: Vec<_> = reader
                 .lines()
                 .map(|result| result.unwrap_or_else(|_| String::new()))
                 .take_while(|line| !line.is_empty())
-                .collect();
+                .collect(); 
 
-            println!("Request: {http_request:#?}");
+            let res = Listener::parse_http_request(&http_request[0]);
+            match res {
+                None => return Err(Error::msg("couldn't parse the HTTP")),
+                _ => ()
+            }
+
+            let (path, options) = res.unwrap();
+            let handler = self.choose_handler(&path);
+
+            let http_return_dict = match handler {
+                None => Map::new(),
+                _ => handler.unwrap()(options)
+            };
+
+            let http_return = to_string(&http_return_dict)?;
+
+            let http_str = format!(
+                "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n{http_return}"
+            );
+
+            let _ = stream.write(http_str.as_bytes());
+
+            println!("{path}");
         }
         Ok(())
     }
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,20 @@ mod config;
 mod listener;
 
 use anyhow::Result;
-use std::path::Path;
+use std::{collections::HashMap, path::Path};
+
+fn test_handler(_map: HashMap<String, String>) -> HashMap<String, String> {
+    HashMap::from([
+        ("hello".to_string(), "world".to_string())
+    ])
+}
 
 fn main() -> Result<()> {
     let config = config::Config::new(Path::new("cofy_config.toml"))?;
     println!("IP: {}:{}", config.ip, config.port);
-    let listener = listener::Listener::new(&config.ip, config.port);
+    let mut listener = listener::Listener::new(&config.ip, config.port)?;
+    listener.attach_handler("/".to_string(), test_handler);
 
-    listener?.handle_connections()?;
+    listener.handle_connections()?;
     Ok(())
 }
