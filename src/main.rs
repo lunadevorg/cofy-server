97badd3fec06eb24644a2315c87cc4018c952681
ognevnydemon <maksapple2306@gmail.com>
2024-05-12 11:42:42 +0300

    use anyhow::with_context

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,25 @@ mod config;
 mod http_parse;
 mod listener;
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use log::info;
 use std::path::Path;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let config = config::Config::new(Path::new("cofy_config.toml"))?;
+    let config = config::Config::new(Path::new("cofy_config.toml"))
+        .with_context(|| "Failed to load config file")?;
     let ip = format!("{}:{}", config.ip, config.port);
     info!("{ip}");
 
-    let mut listener = listener::Listener::new(ip).await?;
-    listener.main_loop().await?;
+    let mut listener = listener::Listener::new(ip)
+        .await
+        .with_context(|| "Failed to create listener")?;
+
+    listener
+        .main_loop()
+        .await
+        .with_context(|| "Failed to run loop")?;
 
     Ok(())
 }
