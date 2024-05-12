090532196eb1550a0e4d9dacb2d7c0c97039d6fb
sillygabe <gabrielonline630@gmail.com>
2024-05-12 12:39:20 +0300

    Works (please end my misery)

--- src/listener.rs
+++ src/listener.rs
@@ -1,31 +1,27 @@
 //use std::collections::HashMap;
 use anyhow::Result;
 use log::{error, info};
-use serde_json::to_string;
 use std::collections::HashMap;
 use tokio::net::TcpListener;
 
 use crate::http_parse::{self, parse_http_request};
 
-//type StringMap = HashMap<String, String>;
-//type Handler = fn(StringMap, String) -> StringMap;
+pub type StringMap = HashMap<String, String>;
 
 pub struct Listener {
     listener: TcpListener,
-    //handlers: HashMap<String, Handler>,
 }
 
 impl Listener {
     pub async fn new(ip: String) -> Result<Self> {
         Ok(Self {
             listener: TcpListener::bind(ip).await?,
-            //handlers: HashMap::new(),
         })
     }
 
-    //pub fn attach_handler(&mut self, path: String, handler: Handler) {
-    //    self.handlers.insert(path, handler);
-    //}
+    async fn handle_request(_path: String, args: StringMap) -> String {
+        http_parse::construct_response(200, &args)
+    }
 
     pub async fn main_loop(&mut self) -> Result<()> {
         loop {
@@ -42,17 +38,12 @@ impl Listener {
             let data = String::from_utf8_lossy(&buffer).into_owned();
 
             let (path, args) = parse_http_request(&data).unwrap_or_default();
-            info!("{}", to_string(&args)?);
-            let response = HashMap::from([("path".to_owned(), path)]);
-
-            stream.writable().await?;
-            let response = http_parse::construct_response(200, &response);
-            info!("{response}");
-
-            let result = stream.try_write(response.as_bytes()).unwrap_or_default();
+            let result = tokio::spawn(Self::handle_request(path, args));
+            let result = result.await.unwrap();
+            let size = stream.try_write(result.as_bytes()).unwrap_or_default();
             assert_eq!(
-                result,
-                response.len(),
+                size,
+                result.len(),
                 "the data written in stream must have the same length as the response"
             );
         }
