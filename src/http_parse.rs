033b8bf61b9ecd2b2b96ec671de8a89a33dbebe5
Maksim Bondarenkov <maksapple2306@gmail.com>
2024-05-23 14:19:58 +0300

    refactor http_parse

--- src/http_parse.rs
+++ src/http_parse.rs
@@ -15,30 +15,30 @@ use std::collections::HashMap;
 type StringMap = HashMap<String, String>;
 
 pub fn parse_http_request(request: &str) -> Option<(String, StringMap)> {
-    let parts: Vec<&str> = request.splitn(2, ' ').collect();
+    let parts: Vec<_> = request.splitn(2, ' ').collect();
     if parts.len() != 2 {
         return None;
     }
 
     let (path_and_query, _version) = (parts[1].split_once(' ')?.0, parts[0]);
 
-    let path: String;
     let mut query_params = HashMap::new();
 
-    if let Some(index) = path_and_query.find('?') {
-        path = path_and_query.get(..index)?.to_owned();
-        let query = &path_and_query.get(index + 1..)?;
+    let path = if let Some(index) = path_and_query.find('?') {
+        let query = path_and_query.get(index + 1..)?;
 
         for param in query.split('&') {
             let param = param.replace("%20", " ");
-            let key_value: Vec<&str> = param.split('=').collect();
+            let key_value: Vec<_> = param.split('=').collect();
             if key_value.len() == 2 {
                 query_params.insert(key_value[0].to_owned(), key_value[1].to_owned());
             }
         }
+
+        path_and_query.get(..index)?.to_owned()
     } else {
-        path = path_and_query.to_owned();
-    }
+        path_and_query.to_owned()
+    };
 
     Some((path, query_params))
 }
